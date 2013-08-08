7e32dc4262212f3ae9032a8d465aae18ead9fe9f
Lei Shi <selear@gmail.com>
2013-08-08 15:28:30 +0800

    Javascript基础教程第七章, 提取字符串

---
+++ JavaScript/Chapter07/extractString/script03.js
@@ -0,0 +1,52 @@
+window.onload = initFroms;
+window.onunload = function(){}
+
+function initFroms() {
+	for(var i = 0; i < document.forms.length; i++) {
+		document.forms[i].onsubmit = validForm;
+	}
+}
+
+function validForm() {
+	var allTags = document.getElementsByTagName("*");
+
+	for(var i = 0; i < allTags.length; i++) {
+		validTag(allTags[i]);
+	}
+	return false;
+
+	function validTag(thisTag) {
+		var allClasses = thisTag.className.split(" ");
+
+		for(var j = 0; j < allClasses.length; j++) {
+			validBasedOnClass(allClasses[j]);
+		}
+
+		function validBasedOnClass(thisClass) {
+			switch(thisClass) {
+				case "":
+					break;
+				case "nameList":
+					thisTag.value = nameList(thisTag.value);
+				default:
+			}
+		}
+
+		function nameList(inNameList) {
+			var newNames = new Array;
+			var newNameFields = "";
+
+			var re = /\s*\n\s*/;
+			var nameList = inNameList.split(re);
+
+			re = /(\S+)\s(\S+)/;
+			for(var k = 0; k < nameList.length; k++) {
+				newNames[k] = nameList[k].replace(re, "$2, $1");
+			}
+			for(k = 0; k < newNames.length; k++) {
+				newNameFields += newNames[k] + "\n";
+			}
+			return newNameFields;
+		}
+	}
+}
