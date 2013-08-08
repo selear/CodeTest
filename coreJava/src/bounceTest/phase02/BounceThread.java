29a59409b6fe73bd98b2f5120676058721e4b014
Lei Shi <selear@gmail.com>
2013-08-08 16:49:17 +0800

    改进了原先的程序, 额外建立了BounceThread线程用于处理球移动的操作
    每次start都加入新的小球. 同事能够在小球运行过程中点击close跳出

---
+++ coreJava/src/bounceTest/phase02/BounceThread.java
@@ -0,0 +1,113 @@
+package bounceTest.phase02;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import bounceTest.phase01.Ball;
+import bounceTest.phase01.BallComponent;
+
+public class BounceThread {
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				JFrame frame = new BounceFrame();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		});
+	}
+
+}
+
+/**
+ * 球移动的执行类, 继承了Runnable接口, 作为单独线程执行
+ * 
+ * @author joylee
+ * 
+ */
+class BallRunnable implements Runnable {
+
+	public BallRunnable(Ball aBall, Component aComponent) {
+		ball = aBall;
+		component = aComponent;
+	}
+
+	@Override
+	public void run() {
+
+		try {
+			for (int i = 1; i <= STEPS; i++) {
+				ball.move(component.getBounds());
+				component.repaint();
+				Thread.sleep(DELAY);
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Ball ball;
+	private Component component;
+	public static final int STEPS = 1000;
+	public static final int DELAY = 5;
+}
+
+class BounceFrame extends JFrame {
+
+	public BounceFrame() {
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		setTitle("BounceThread");
+
+		comp = new BallComponent();
+		add(comp, BorderLayout.CENTER);
+		JPanel buttonPanel = new JPanel();
+		addButton(buttonPanel, "Start", new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				addBall();
+			}
+		});
+
+		addButton(buttonPanel, "Close", new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		add(buttonPanel, BorderLayout.SOUTH);
+	}
+
+	public void addButton(Container c, String title,
+			ActionListener actionListener) {
+		JButton button = new JButton(title);
+		c.add(button);
+		button.addActionListener(actionListener);
+	}
+
+	protected void addBall() {
+		Ball b = new Ball();
+		comp.add(b);
+		Runnable r = new BallRunnable(b, comp);
+		Thread t = new Thread(r);
+		t.start();
+	}
+
+	private BallComponent comp;
+	public static final int DEFAULT_WIDTH = 450;
+	public static final int DEFAULT_HEIGHT = 350;
+	public static final int STEPS = 1000;
+	public static final int DELAY = 3;
+}
