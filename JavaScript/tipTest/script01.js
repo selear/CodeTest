f820527ac1478f95cd27a3361d1bd0828d3ff0a3
Lei Shi <selear@gmail.com>
2013-09-03 16:44:01 +0800

    提示测试

---
+++ JavaScript/tipTest/script01.js
@@ -0,0 +1,25 @@
+// document.onmousemove = showTip;
+// document.onunload = function() {}
+
+// function showTip(e) {
+// 	if(!e) {
+// 		var e = window.event;
+// 	}
+// }
+
+// document.onmousemove = position;
+
+
+// function position(e) {
+// 	if(!e) {
+// 		var e = window.event;
+// 	}
+// 	var d = document.getElementById('tip');
+// 	d.style.visibility = 'visible';
+// 	d.style.left = e.screenX + 'px';
+// 	d.style.top = e.screenY + 'px';
+// }
+
+// function hide() {
+// 	document.getElementById('tip').style.visibility = 'hidden';
+// }
