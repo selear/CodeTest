9e0846441bb09be8f0562420db0746ec7db5891c
selear <selear@gmail.com>
2013-11-06 22:13:31 +0800

    改进了代码, 做出了一些小修正

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,29 +1,46 @@
 var days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+var workdays = "一二三四五六日";
 
-var firstDay = new Date(2013, 7, 1);
-var lastDay = new Date(2013, 8, 0);
+//var firstDay = new Date(2013, 7, 1);
+//var lastDay = new Date(2013, 8, 0);
 
-window.onload = function() {
-	var today = new Date();
+window.onload = createCalendar;
 
-	var year = today.getFullYear();
-	var month = today.getMonth();
-	var day = today.getDay();
+function createCalendar() {
+	//在标题位置插入日期
+	insertDate("date");
 
-	console.log("FullYear : " + today.getFullYear());
-	console.log("Year : " + today.getYear());
-	console.log("Month : " + today.getMonth());
-	console.log("Date : " + today.getDate());
-	console.log("Day : " + today.getDay());
+	//构建整个日历
+	constructCalendar("calendar", "days");
 
-	var daysInMonth = days[month];
-	var dateStr = year + "年" + month + "月" + day + "日";
-	console.log(dateStr);
+	function insertDate(objId) {
 
-	var title = document.getElementById("date");
-	var content = document.createTextNode(dateStr); //document.createTextNode(#string#)属于标准的字符串
-	title.appendChild(content);
+		//获取今天日期信息
+		var today = new Date();
+		var year = today.getFullYear();
+		var month = today.getMonth();
+		var day = today.getDate();
 
-	var calendar = document.getElementById("calendar");
+		/* console.log("FullYear : " + today.getFullYear()); //直接得出当年年份
+		console.log("Year : " + today.getYear()); //从1900年开始计算
+		console.log("Month : " + today.getMonth()); //获取当前月份, 比实际值要小1
+		console.log("Date : " + today.getDate()); //获取当前日期
+		console.log("Day : " + today.getDay()); //获取当前星期几 */
 
+		var daysInMonth = days[month];
+		var dateStr = year + "年" + (month + 1) + "月" + day + "日 星期" + workdays.charAt(today.getDay() - 1);
+		console.log(dateStr);
+
+		//创建文字节点, 同时获取dateObj的对象, 将文字节点插入到对象当中
+		var content = document.createTextNode(dateStr); //document.createTextNode(#string#)属于标准的字符串
+		var dateObj = document.getElementById(objId);
+		dateObj.appendChild(content);
+	}
+
+	function constructCalendar(weekdayId, daysId) {
+		var weekdayObj = document.getElementById(weekdayId);
+		var daysObj = document.getElementById(daysId);
+
+		
+	}
 }
